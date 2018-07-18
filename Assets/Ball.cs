cb5e7a060418adf9d49dcb41a468b64d34475203
jtyborski <jtyborski@gmail.com>
2018-07-18 14:24:52 -0500

    files

---
+++ Assets/Ball.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+   public GameObject wall;
+    public GameObject hole;
+    public Material LightUpMat;
+   public Material StartMaterial;
+
+    void Start()
+    {
+      
+    }
+
+        void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "hole")
+        {
+            wall.GetComponent<MeshCollider>().enabled = false;
+            hole.GetComponentInChildren<Renderer>().sharedMaterial = LightUpMat;
+
+        }
+    }
+        void OnTriggerExit(Collider other)
+        {
+            if (other.tag == "hole")
+            {
+                wall.GetComponent<MeshCollider>().enabled = true;
+                hole.GetComponentInChildren<Renderer>().sharedMaterial = StartMaterial;
+            }
+        }
+    }
+
